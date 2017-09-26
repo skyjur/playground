526348fc38338029764d7521ca56281454c0c1b6
Ski <skyjur@gmail.com>
2017-09-26 10:52:42 +0800

    Implemented authorization through login-with-github

--- github-app/src/server.ts
+++ github-app/src/server.ts
@@ -1,9 +1,11 @@
 import * as dotenv from 'dotenv';
 import * as express from 'express';
+import * as CookieParser from 'cookie-parser';
 import {env} from 'process';
 import * as querystring from 'querystring';
 import * as request from 'request-promise-native';
-import {envGet} from './utils';
+import {envGet, Crypto} from './utils';
+import * as path from 'path';
 
 
 dotenv.config();
@@ -23,8 +25,14 @@ function debugRequest(req: express.Request, res: express.Response, next) {
 export class App {
     app = express();
     port = env['PORT'] || '8000';
+    crypt = new Crypto(CLIENT_SECRET);
 
+    
     constructor(settings?: Partial<App>) {
+        this.app.use(CookieParser());
+        this.app.set('views', __dirname);
+        this.app.use('almond.js', express.static(path.join(__dirname, '../node_modules/almond/almond.js')));
+        this.app.use('/static/', express.static(path.join(__dirname, 'static')));
         this.initRoutes(this.app);
         if(settings) {
             for(let key in settings) { this[key] = settings[key]; }
@@ -32,9 +40,11 @@ export class App {
     }
 
     private initRoutes(app: express.Application) {
-        app.route('/github-callback').all(githubAuth);
+        app.route('/github-callback').all(this.githubAuthCallback);
         app.route('/github-webhook').all(debugRequest);
-        app.route('/github-setup').all(githubSetup);
+        app.route('/github-setup').all(this.githubLoginRedirect);
+        app.route('/logout').all(this.logout);
+        app.route('/').get(this.index);
     }
 
     start() {
@@ -42,31 +52,56 @@ export class App {
             console.log(`Running web server on ${this.port}`);
         });
     }
+    
+    githubLoginRedirect = (req: express.Request, res: express.Response) => {
+        var qs = querystring.stringify({
+            'client_id': CLIENT_ID,
+            'redirect_uri': 'https://testbed.skijur.com/github-callback',
+            'state': 123
+        })
+        res.redirect('http://github.com/login/oauth/authorize?' + qs)
+        res.end();
+    };
 
-
-}
-
-function githubSetup(req: express.Request, res: express.Response) {
-    var qs = querystring.stringify({
-        'client_id': CLIENT_ID,
-        'redirect_uri': 'https://testbed.skijur.com/github-callback',
-        'state': 123
-    })
-    res.redirect('http://github.com/login/oauth/authorize?' + qs)
-    res.end();
-}
-
-async function githubAuth(req: express.Request, res: express.Response) {
-    var code = req.query.code;
-    let data = await request.post('https://github.com/login/oauth/access_token?', {
-        form: {
-            client_id: CLIENT_ID,
-            client_secret: CLIENT_SECRET,
-            code: code
+    index = async (req: express.Request, res: express.Response) => {
+        if(req.cookies && req.cookies.token) {
+            res.render('./index.pug', {
+                token: req.cookies.token
+            });
+        } else {
+            res.render('./login.pug');
         }
-    });
-    res.json(querystring.parse(data)).end();
+    };
+
+    githubAuthCallback = async (req: express.Request, res: express.Response) => {
+        var code = req.query.code;
+        let data = await request.post('https://github.com/login/oauth/access_token?', {
+            form: {
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+                code: code
+            }
+        });
+        let parsedData = querystring.parse(data);
+        if(!parsedData.access_token) {
+            throw new Error(data);
+        }
+        res.cookie('token', this.crypt.encrypt(parsedData.access_token), {
+            httpOnly: true,
+            expires: false
+        });
+        res.redirect('/');
+        res.end();
+    };
+
+    logout = (req: express.Request, res: express.Response) => {
+        res.clearCookie('token');
+        res.redirect('/');
+        res.end();
+    }
 }
+
+
 
 if (require.main === module) {
     new App().start();
--- github-app/src/utils.ts
+++ github-app/src/utils.ts
@@ -1,4 +1,5 @@
 import {env, exit, stderr} from 'process';
+import * as crypto from 'crypto';
 
 
 export function envGet(key: string): string {
@@ -7,5 +8,20 @@ export function envGet(key: string): string {
     } else {
         stderr.write(`Missing environment variable: ${key}`);
         exit(1);
+    }
+}
+
+
+export class Crypto {
+    constructor(private password: string) {}
+    
+    encrypt(value: string) {
+        let cipher = crypto.createCipher('aes-256-ctr', this.password);
+        return cipher.update(value, 'utf8', 'hex') + cipher.final('hex');
+    }
+    
+    decrypt(value: string) {
+        let decipher = crypto.createDecipher('aes-256-ctr', this.password);
+        return decipher.update(value, 'hex', 'utf8') + decipher.final('utf8');
     }
 }
